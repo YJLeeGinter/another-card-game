d82d08a6673c88b264691127de75a47a37188c25
Yeon Lee <ginterlee29@gmail.com>
2019-07-30 13:34:31 -0400

    added the initial setting

---
+++ game.js
@@ -0,0 +1,12 @@
+var rivalHero = document.getElementById('rival-hero');
+var myHero = document.getElementById('my-hero');
+var rivalDeck = document.getElementById('rival-deck');
+var myDeck = document.getElementById('my-deck');
+
+function initialSetup(){
+
+
+
+}
+
+initialSetup();
