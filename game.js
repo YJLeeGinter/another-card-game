aaed7fe3fd7fc66d9b9f8db470789e095993a4f9
Yeon Lee <ginterlee29@gmail.com>
2019-07-31 12:40:10 -0400

    added a button to change tunrs

--- game.js
+++ game.js
@@ -2,10 +2,18 @@ var rivalHero = document.getElementById('rival-hero');
 var myHero = document.getElementById('my-hero');
 var rivalDeck = document.getElementById('rival-deck');
 var myDeck = document.getElementById('my-deck');
+var rivalField = document.getElementById('rival-cards');
+var myField = document.getElementById('my-cards');
+var rivalCost = document.getElementById('rival-cost');
+var myCost = document.getElementById('my-cost');
+var turnBtn = document.getElementById('turn-btn');
 var rivalDeckData = [];
-var myDeckData = [];
+var myDeckData = []; 
 var rivalHeroData;
 var myHeroData;
+var rivalFieldData = [];
+var myFieldData = [];
+var turn = true; // true for my turn
 
 function connectCardAndDom(data, dom, hero){
     var card = document.querySelector('.card-hidden .card').cloneNode(true);
@@ -18,6 +26,53 @@ function connectCardAndDom(data, dom, hero){
         name.textContent = 'Hero';
         card.appendChild(name);
     }
+    card.addEventListener('click', function(){
+        if(turn){ //  my turn
+            if(!data.mine){ // but clicked the rival's card
+                return;
+            }
+            var currentCost = Number(myCost.textContent);
+            if(currentCost < data.cost){
+                return;
+            }
+            if(data)    
+            var idx = myDeckData.indexOf(data);
+            myDeckData.splice(idx,1);
+            myFieldData.push(data);
+            console.log(myDeckData, myFieldData);
+            myDeck.innerHTML = '';
+            myField.innerHTML = '';
+            myFieldData.forEach(function(data){
+                connectCardAndDom(data, myField);
+            });
+            myDeckData.forEach(function(data){
+                connectCardAndDom(data, myDeck);
+            });
+         myCost.textContent = currentCost - data.cost;
+        } else{ // rival's turn
+         if(data.mine){ // but clicked my turn
+             return;
+         }
+         var currentCost = Number(rivalCost.textContent);
+         if(currentCost < data.cost){
+             return;
+         }
+            var idx = rivalDeckData.indexOf(data);
+            rivalDeckData.splice(idx,1);
+            rivalFieldData.push(data);
+            rivalDeck.innerHTML = '';
+            rivalField.innerHTML = '';
+            rivalFieldData.forEach(function(data){
+                connectCardAndDom(data, rivalField);
+            });
+            rivalDeckData.forEach(function(data){
+                connectCardAndDom(data, rivalDeck);
+            });
+            myCost.textContent = currentCost - data.cost;
+
+        }
+    });
+
     dom.appendChild(card);
 }
 
@@ -32,7 +87,7 @@ function createRivalDeck(num){
 
 function createMyDeck(num){
     for(var i = 0; i < num ; i++){
-        myDeckData.push(makeCards());
+        myDeckData.push(makeCards(false, true));
     }
     myDeckData.forEach(function(data){
         connectCardAndDom(data, myDeck);
@@ -40,7 +95,7 @@ function createMyDeck(num){
 }
 
 function createMyHero(){
-   myHeroData =  makeCards(true);
+   myHeroData =  makeCards(true, true);
    connectCardAndDom(myHeroData, myHero, true);
 }
 
@@ -49,7 +104,7 @@ function createRivalHero(){
    connectCardAndDom(rivalHeroData, rivalHero, true);
 }
 
-function Card(hero){  
+function Card(hero, myCard){  
     if(hero){
         this.att = Math.ceil(Math.random()* 2);
         this.hp = Math.ceil(Math.random()*5)+25;
@@ -60,11 +115,15 @@ function Card(hero){
         this.hp = Math.ceil(Math.random()*5);
         this.cost = Math.floor((this.att + this.hp) /2);
     }
+
+    if(myCard){
+        this.mine = true;
+    }
       
 }
 
-function makeCards(hero){
-    return new Card(hero);
+function makeCards(hero, myCard){
+    return new Card(hero, myCard);
 }
 
 function initialSetup(){
@@ -75,5 +134,11 @@ function initialSetup(){
     createRivalHero();
 
 }
+
+turnBtn.addEventListener('click', function(){
+    turn = !turn;
+    document.getElementById('rival').classList.toggle('turn');
+    document.getElementById('my').classList.toggle('turn');
+});
 
 initialSetup(); 
