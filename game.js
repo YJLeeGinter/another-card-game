1b5f4778bb35742c8ab17fcfff8a7505c4f797fc
Yeon Lee <ginterlee29@gmail.com>
2019-08-01 00:31:47 -0400

    finished making the game

--- game.js
+++ game.js
@@ -1,19 +1,119 @@
-var rivalHero = document.getElementById('rival-hero');
-var myHero = document.getElementById('my-hero');
-var rivalDeck = document.getElementById('rival-deck');
-var myDeck = document.getElementById('my-deck');
-var rivalField = document.getElementById('rival-cards');
-var myField = document.getElementById('my-cards');
-var rivalCost = document.getElementById('rival-cost');
-var myCost = document.getElementById('my-cost');
+var rival = {
+    hero : document.getElementById('rival-hero'),
+    deck : document.getElementById('rival-deck'),
+    field : document.getElementById('rival-cards'),
+    cost : document.getElementById('rival-cost'),
+    deckData : [],
+    heroData : [],
+    fieldData : [],
+    slectedCard : null,
+    selectedCardData: null,
+}
+
+var me = {
+    hero : document.getElementById('my-hero'),
+    deck : document.getElementById('my-deck'),
+    field : document.getElementById('my-cards'),
+    cost : document.getElementById('my-cost'),
+    deckData : [],
+    heroData : [],
+    fieldData : [],
+    selectedCard: null,
+    selectedCardData : null,
+}
+
 var turnBtn = document.getElementById('turn-btn');
-var rivalDeckData = [];
-var myDeckData = []; 
-var rivalHeroData;
-var myHeroData;
-var rivalFieldData = [];
-var myFieldData = [];
 var turn = true; // true for my turn
+
+function moveDeckToField(data, myTurn){
+    
+    var obj = myTurn? me : rival;
+   
+    var currentCost = Number(obj.cost.textContent);
+    if(currentCost < data.cost){
+    return 'end';
+    }
+
+    var idx = obj.deckData.indexOf(data);
+    obj.deckData.splice(idx,1);
+    obj.fieldData.push(data);
+    drawFieldAgain(obj);
+    drawDeckAgain(obj);
+    data.field = true; // 참조관계 이용
+    obj.cost.textContent = currentCost - data.cost;
+}
+
+function drawFieldAgain(obj){
+    obj.field.innerHTML = '';
+    obj.fieldData.forEach(function(data){
+        connectCardAndDom(data, obj.field);
+    });
+}
+
+function drawDeckAgain(obj){
+    obj.deck.innerHTML = '';
+    obj.deckData.forEach(function(data){
+        connectCardAndDom(data, obj.deck);
+        });
+}
+
+function drawHeroAgain(obj){
+    obj.hero.innerHTML = '';
+    connectCardAndDom(obj.heroData, obj.hero, true);
+}
+
+function drawScreenAgain(myScreen){
+    var obj = myScreen? me : rival;
+    drawFieldAgain(obj);
+    drawDeckAgain(obj);
+    drawHeroAgain(obj);
+}
+
+function performTurnAction(card, data, myTurn){
+    var ally = myTurn? me : rival;
+    var enemy = myTurn? rival: me;
+    if(card.classList.contains('card-turnover')){
+        return;
+    }
+
+    var enemyCard = myTurn? !data.mine : data.mine;
+    if( enemyCard && ally.selectedCard ){ 
+        // but I clicked rival card, my card is selected and 턴이 끝난 카드가 아니면 상대를 공격
+        data.hp = data.hp - ally.selectedCardData.att;
+        if(data.hp <=0){ // when the card is dead
+            var index = enemy.fieldData.indexOf(data);
+            if(index > -1){
+                enemy.fieldData.splice(index,1);
+            } else{ // when the hero is dead
+                alert('You won!');
+            initialSetup();
+            }
+        }
+        drawScreenAgain(!myTurn);
+        ally.selectedCard.classList.remove('card-selected');
+        ally.selectedCard.classList.add('card-turnover');
+        ally.selectedCard = null;
+        ally.selectedCardData = null;
+        return;
+    } else if(enemyCard){ // but clicked the rival's card or when a card is in the field
+    return;
+   }
+
+if(data.field){  // when the card is on the field
+    card.parentNode.querySelectorAll('.card-selected').forEach(function(card){
+            card.classList.remove('card-selected');
+        });
+    card.classList.add('card-selected');
+    ally.selectedCard = card;
+    ally.selectedCardData = data;
+
+} else{ // when the card is on the deck
+    
+    if(moveDeckToField(data, myTurn) !== 'end') {
+       myTurn? createMyDeck(1) : createRivalDeck(1);   
+      }
+} 
+}
 
 function connectCardAndDom(data, dom, hero){
     var card = document.querySelector('.card-hidden .card').cloneNode(true);
@@ -27,50 +127,7 @@ function connectCardAndDom(data, dom, hero){
         card.appendChild(name);
     }
     card.addEventListener('click', function(){
-        if(turn){ //  my turn
-            if(!data.mine){ // but clicked the rival's card
-                return;
-            }
-            var currentCost = Number(myCost.textContent);
-            if(currentCost < data.cost){
-                return;
-            }
-            if(data)    
-            var idx = myDeckData.indexOf(data);
-            myDeckData.splice(idx,1);
-            myFieldData.push(data);
-            console.log(myDeckData, myFieldData);
-            myDeck.innerHTML = '';
-            myField.innerHTML = '';
-            myFieldData.forEach(function(data){
-                connectCardAndDom(data, myField);
-            });
-            myDeckData.forEach(function(data){
-                connectCardAndDom(data, myDeck);
-            });
-         myCost.textContent = currentCost - data.cost;
-        } else{ // rival's turn
-         if(data.mine){ // but clicked my turn
-             return;
-         }
-         var currentCost = Number(rivalCost.textContent);
-         if(currentCost < data.cost){
-             return;
-         }
-            var idx = rivalDeckData.indexOf(data);
-            rivalDeckData.splice(idx,1);
-            rivalFieldData.push(data);
-            rivalDeck.innerHTML = '';
-            rivalField.innerHTML = '';
-            rivalFieldData.forEach(function(data){
-                connectCardAndDom(data, rivalField);
-            });
-            rivalDeckData.forEach(function(data){
-                connectCardAndDom(data, rivalDeck);
-            });
-            myCost.textContent = currentCost - data.cost;
-
-        }
+        performTurnAction(card, data, turn);        
     });
 
     dom.appendChild(card);
@@ -78,30 +135,26 @@ function connectCardAndDom(data, dom, hero){
 
 function createRivalDeck(num){
     for(var i = 0; i < num ; i++){
-        rivalDeckData.push(makeCards());
+        rival.deckData.push(makeCards());
     }
-    rivalDeckData.forEach(function(data){
-        connectCardAndDom(data, rivalDeck);
-    })
+   drawDeckAgain(rival);
 }
 
 function createMyDeck(num){
     for(var i = 0; i < num ; i++){
-        myDeckData.push(makeCards(false, true));
+        me.deckData.push(makeCards(false, true));
     }
-    myDeckData.forEach(function(data){
-        connectCardAndDom(data, myDeck);
-    }) 
+    drawDeckAgain(me);
 }
 
 function createMyHero(){
-   myHeroData =  makeCards(true, true);
-   connectCardAndDom(myHeroData, myHero, true);
+   me.heroData =  makeCards(true, true);
+   connectCardAndDom(me.heroData, me.hero, true);
 }
 
 function createRivalHero(){
-    rivalHeroData = makeCards(true);
-   connectCardAndDom(rivalHeroData, rivalHero, true);
+    rival.heroData = makeCards(true);
+   connectCardAndDom(rival.heroData, rival.hero, true);
 }
 
 function Card(hero, myCard){  
@@ -109,6 +162,7 @@ function Card(hero, myCard){
         this.att = Math.ceil(Math.random()* 2);
         this.hp = Math.ceil(Math.random()*5)+25;
         this.hero = true;
+        this.field = true;
 
     } else{
         this.att = Math.ceil(Math.random()* 5);
@@ -126,19 +180,32 @@ function makeCards(hero, myCard){
     return new Card(hero, myCard);
 }
 
-function initialSetup(){
+function initialSetup(){ 
 
     createRivalDeck(5);
     createMyDeck(5);
     createMyHero();
     createRivalHero();
-
+    drawScreenAgain(true);
+    drawScreenAgain(false);
 }
 
 turnBtn.addEventListener('click', function(){
-    turn = !turn;
-    document.getElementById('rival').classList.toggle('turn');
+    var obj = turn? me : rival;
+
+    drawFieldAgain(obj);    
+    drawHeroAgain(obj);
+
+    turn = !turn; // code to change tunrs
+    if(turn){
+        me.cost.textContent = 10;
+    } else{
+        rival.cost.textContent = 10;
+    }
+    document.getElementById('rival').classList.toggle('turn');    
     document.getElementById('my').classList.toggle('turn');
+    
+   
 });
 
 initialSetup(); 
